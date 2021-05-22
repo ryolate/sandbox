2616537e0a2eb57bf7d3537edd0a1966edee7f4a
ryo <whatever@example.com>
2021-05-22 19:36:54 +0900

    Add router

--- index.tsx
+++ index.tsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import { Demo } from './sandbox'
 
+
+import {
+    HashRouter as Router,
+    Link, Route, Switch
+} from 'react-router-dom'
+
 function run(canvas: HTMLCanvasElement) {
     return
 }
@@ -11,7 +17,37 @@ const Game = () => {
     return <div></div>
 }
 
+function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/sandbox">Sandbox</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+                <Switch>
+                    <Route path="/sandbox">
+                        <Demo />
+                    </Route>
+                    <Route path="/">
+                        <h2>Home</h2>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    )
+}
+
 ReactDOM.render(
-    <Demo />,
+    <App />,
     document.getElementById('app'),
 )
